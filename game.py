4cae286da9d37eaf156e0435a5190136a1054b7a
MulahTeni <melihtunaipek.mti@gmail.com>
2023-08-08 18:23:03 +0300

    dinner time

--- game.py
+++ game.py
@@ -1,31 +1,48 @@
 # Example file showing a circle moving on screen
 import pygame
+import random
 
-# pygame setup
+
+# Player class
+class Player(object):
+    def __init__(self, color, pos):
+        self.rect = pygame.Rect(pos[0], pos[1], 20, 120)
+        self.color = color
+
+    def move(self, d):
+        self.rect.y += d
+
+# Ball class
+class Ball(object):
+    def __init__(self):
+        self.cir = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+        self.radius = 15
+
+# Outer Frame
+class Frame(object):
+    def __init__(self):
+        self.upper = pygame.Rect(0, 0, screen.get_width(), 10)
+        self.lower = pygame.Rect(0, screen.get_height()-9, screen.get_width(), 10)
+
+        self.left = pygame.Rect(0, 0, 10, screen.get_height())
+        self.right = pygame.Rect(screen.get_width()-9, 0, 10, screen.get_height())
+
+
+# Game setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
-class Player(object):
+player1 = Player("red", (40, screen.get_height()/2 - 60))
+player2 = Player("blue", (screen.get_width() - 60, screen.get_height()/2 - 60))
 
-    def __init__(self, color, pos):
-        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)#pygame.Rect(32, 32, 16, 16)
-        self.color = color
+ball = Ball()
 
-    def move(self, d):
-
-        # Move each axis separately. Note that this checks for collisions both times.
-        self.rect.y += d
+frame = Frame()
 
 
-player1_pos = pygame.Vector2(41, screen.get_height()/2 - 50)
-player2_pos = pygame.Vector2(screen.get_width() - 41, screen.get_height()/2 - 50)
-target_pos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
-
-player1 = Player("red", player1_pos)
-player2 = Player("blue", player2_pos)
 
 while running:
     # poll for events
@@ -37,9 +54,7 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
 
-    #pygame.draw.rect(screen, "red", (player1_pos[0], player1_pos[1], 20, 100))
-    #pygame.draw.rect(screen, "blue", (player2_pos[0], player2_pos[1], 20, 100))
-    pygame.draw.circle(screen, "black", target_pos, 40)
+
 
     keys = pygame.key.get_pressed()
 
@@ -47,17 +62,25 @@ while running:
         running = False
 
     if keys[pygame.K_w]:
-        player1.move(-2)
+        player1.move(-300 * dt)
     if keys[pygame.K_s]:
-        player1.move(2)
+        player1.move(300 * dt)
 
     if keys[pygame.K_UP]:
-        player2.move(-2)
+        player2.move(-300 * dt)
     if keys[pygame.K_DOWN]:
-        player2.move(2)
+        player2.move(300 * dt)
+
+
+    pygame.draw.rect(screen, "purple", frame.upper)
+    pygame.draw.rect(screen, "purple", frame.lower)
+    pygame.draw.rect(screen, "green", frame.left)
+    pygame.draw.rect(screen, "green", frame.right)
 
     pygame.draw.rect(screen, player1.color, player1.rect)
     pygame.draw.rect(screen, player2.color, player2.rect)
+
+    pygame.draw.circle(screen, "black", ball.cir, ball.radius)
     # flip() the display to put your work on screen
     pygame.display.flip()
 
